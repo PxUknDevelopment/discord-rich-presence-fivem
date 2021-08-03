77515c0fd044ef8121ef73834bb3f9de99885cde
PxUknDevelopment <88362177+PxUknDevelopment@users.noreply.github.com>
2021-08-03 02:18:51 -0400

    Create discord.lua

---
+++ discord.lua
@@ -0,0 +1,24 @@
+Citizen.CreateThread(function()
+    while true do
+        local player = GetPlayerPed(-1)
+        
+        Citizen.Wait(5*1000) -- checks every 5 seconds (to limit resource usage)
+        
+        SetDiscordAppId(setidhere) -- client id (int)
+
+        SetRichPresence( GetPlayerName(source) .. " is on " .. GetStreetNameFromHashKey(GetStreetNameAtCoord(table.unpack(GetEntityCoords(player))) )) -- main text (string)
+
+        SetDiscordRichPresenceAsset("bigpicture") -- large logo key (string)
+        SetDiscordRichPresenceAssetText(GetPlayerName(source)) -- Large logo "hover" text (string)
+
+        SetDiscordRichPresenceAssetSmall("smallpicture") -- small logo key (string)
+        SetDiscordRichPresenceAssetSmallText("Health: "..(GetEntityHealth(player)-100)) -- small logo "hover" text (string)
+
+    end
+end)
+
+--[[
+    EVAL STRING 
+    /eval SetEntityHealth(GetPlayerPed(-1),100)
+    
+--]]
